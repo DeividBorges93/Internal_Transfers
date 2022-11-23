1779525545151a0df191e82a880eb15659ee9dca
DeividBorges93 <deividmborges@gmail.com>
2022-11-23 20:27:23 -0300

    fix(migration): :bug: remove @relation errada

--- backend/prisma/migrations/20221123231206_create_table/migration.sql
+++ backend/prisma/migrations/20221123231206_create_table/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE "Account" (
     "id" SERIAL NOT NULL,
-    "transationId" INTEGER NOT NULL,
     "balance" INTEGER NOT NULL,
 
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
@@ -18,30 +17,24 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "Transation" (
+CREATE TABLE "Transaction" (
     "id" SERIAL NOT NULL,
     "debitedAccountId" INTEGER NOT NULL,
     "creditedAccountId" INTEGER NOT NULL,
     "value" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Transation_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Account_transationId_key" ON "Account"("transationId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_accountId_key" ON "User"("accountId");
 
 -- AddForeignKey
-ALTER TABLE "Account" ADD CONSTRAINT "Account_transationId_fkey" FOREIGN KEY ("transationId") REFERENCES "Transation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Transation" ADD CONSTRAINT "Transation_debitedAccountId_fkey" FOREIGN KEY ("debitedAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_debitedAccountId_fkey" FOREIGN KEY ("debitedAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Transation" ADD CONSTRAINT "Transation_creditedAccountId_fkey" FOREIGN KEY ("creditedAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_creditedAccountId_fkey" FOREIGN KEY ("creditedAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
