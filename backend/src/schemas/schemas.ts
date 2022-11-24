751387b25e0fad5eaba07563ab663885c73377e4
DeividBorges93 <deividmborges@gmail.com>
2022-11-24 18:51:08 -0300

    feat(schemas_zod): :sparkles: implementa schema para validar usuário
    comenta regex para verificação, criando loopin na requisição.

---
+++ backend/src/schemas/schemas.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+// const pattern = '^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]$';
+// const regexExp = new RegExp(pattern);
+
+// console.log(regexExp, ' regex');
+
+
+const numMinUsername = 3;
+const numMinPassword = 8
+
+const messages = {
+  minUsername: "Usuário precisa ter pelo menos 3 caracteres",
+  minLogin: "Senha precisa ter pelo menos 8 caracteres",
+  numAndUpCase: "Senha precisa ter um número e uma letra maiúscula"
+}
+
+export const UserSchema = z.object({
+    username: z.string().min(numMinUsername, messages.minUsername),
+    password: z.string().min(numMinPassword, messages.minLogin), //.regex(regexExp, { message: messages.numAndUpCase }),
+  })
+
+  export type User = z.infer<typeof UserSchema>;
