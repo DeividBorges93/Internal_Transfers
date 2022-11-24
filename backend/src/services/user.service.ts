3b14db7f2c1c59ef97c7a1596c0f1a73715cddff
DeividBorges93 <deividmborges@gmail.com>
2022-11-24 19:22:37 -0300

    feat(services): :sparkles: implementa validações de usuário e encriptamento da senha
    utliza ZOD e BCRYPT para tais funções
    
    altera método de captura de erros para utilizar o middleware de erro.

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,33 +1,48 @@
-import { User, PrismaClient } from '@prisma/client'
-import InvalidUser from '../customErrors/InvalidUser';
+import { PrismaClient } from '@prisma/client'
+import IError from '../interfaces/IError';
+import { User, UserSchema } from '../schemas/schemas';
+import HashPassword from '../utils/hashPassword';
+import IUser from '../interfaces/IUser';
 
 const prisma = new PrismaClient();
 
 export default class UserService {
-  public register = async (user: User): Promise<User | InvalidUser> => {
+  public register = async (user: User): Promise<IUser | IError> => {
+
+    const validatedUser = await UserSchema.safeParseAsync(user)
     
+    if (validatedUser.success === false) throw {code: 401, message: validatedUser.error.issues[0].message} as IError;
+    
+    const initialbalance = 100;
+    
+    const createdAccount = await prisma.account.create({
+      data: {
+        balance: initialbalance,
+      },
+    });
+
     const { username, password } = user;
 
-    if (!username || !password) throw new InvalidUser('Todos os campos são obrigatórios');
+    const hashPassword = new HashPassword();
+
+    const hashedPassword = hashPassword.hash(password, 8);
 
     const userAlreadyExists = await prisma.user.findFirst({
       where: { username }
     });
-    
-    if (userAlreadyExists) throw new InvalidUser('Usuário já existe');
 
-      const createdAccount = await prisma.account.create({
-        data: {
-          balance: 100
-        },
-      });
-      const createdUser = await prisma.user.create({
-        data: {
-          username: username,
-          password: password,
-          accountId: createdAccount.id
-        },
-      });
-      return createdUser;
+    if (userAlreadyExists) throw { code: 401, message: 'Usuário já existe' };
+
+    const createdUser = await prisma.user.create({
+      data: {
+        username: username,
+        password: hashedPassword,
+        accountId: createdAccount.id
+      },
+    });
+
+    const { id, accountId } = createdUser;
+    
+    return { id, username, accountId };
   }
 }
