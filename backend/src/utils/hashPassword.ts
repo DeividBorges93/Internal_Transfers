81eb6a681932fedd48cad38500653a3ce35be3d1
DeividBorges93 <deividmborges@gmail.com>
2022-11-25 00:24:18 -0300

    feat(password): :sparkles: implementa metodo que compara a senha da req com a senha do banco
    usa a função compareSync do bcrypt

--- backend/src/utils/hashPassword.ts
+++ backend/src/utils/hashPassword.ts
@@ -3,5 +3,9 @@ import bcrypt from 'bcrypt';
 export default class HashPassword {
   public hash(password: string, length: number) {
     return bcrypt.hashSync(password, length)
-  }
+  };
+
+  public compareHash(password: string, hasPassword: string) {
+    return bcrypt.compareSync(password, hasPassword)
+  };
 }
