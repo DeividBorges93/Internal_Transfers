d79a3faf3e8ef0c4f7feca7346c7e88997519357
DeividBorges93 <deividmborges@gmail.com>
2022-11-26 14:02:29 -0300

    refactor(middlewares): :recycle: diminui as responsabildiades do authMiddleware
    recebe o authorization, faz o decrpyt e retorna as informações

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,16 +1,9 @@
-import { Request } from 'express';
 import Jwt from '../utils/tokenGenerator';
 
-export default class AuthMiddleware {
-  public validateAuthorization(req: Request) {
+  export default (authorization: string) => {
     const jwt = new Jwt();
-
-    if (!req.headers.authorization) throw { code: 401, message:  'É nescessário um token válido' };
-    
-    const { authorization } = req.headers;
 
     const tokenDecrypt = jwt.decrypt(authorization);
 
     return tokenDecrypt;
   };
-};
