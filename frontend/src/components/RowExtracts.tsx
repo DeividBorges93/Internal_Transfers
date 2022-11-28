fc48df80fde0db1dc11f2b668b86bafac0c2ee5b
DeividBorges93 <deividmborges@gmail.com>
2022-11-28 19:52:38 -0300

    feat(styles): :lipstick: altera forma de renderizar  data e a hora e  o valor

--- frontend/src/components/RowExtracts.tsx
+++ frontend/src/components/RowExtracts.tsx
@@ -1,13 +1,19 @@
 import React from "react"
 
 export default function RowGenerate({ id, debitedAccountId, creditedAccountId, value, createdAt }: cahsOutOrIn) {
+  const valor = `IT$ ${value},00`;
+  const date = new Date(createdAt);
+  const formatedDate = date.toLocaleDateString();
+  const formatedTime = date.toLocaleTimeString();
+  
   return (
-      <tr key={id}>
-        <td>{id}</td>
-        <td>{debitedAccountId}</td>
-        <td>{creditedAccountId}</td>
-        <td>{value}</td>
-        <td>{createdAt}</td>
-      </tr>
-    
-  )}
+    <tr key={id}>
+      <td>{id}</td>
+      <td>{debitedAccountId}</td>
+      <td>{creditedAccountId}</td>
+      <td>{valor}</td>
+      <td>{formatedDate}</td>
+      <td>{formatedTime}</td>
+    </tr>
+  )
+}
