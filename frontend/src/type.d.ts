d4ccf5e1e25a9d98bd59df84002db36fdf2dafc5
DeividBorges93 <deividmborges@gmail.com>
2022-11-28 11:06:27 -0300

    feat(type): :sparkles: adc arquivo type.d.ts com tipos globais
    utiliza eles sem precisar importar

---
+++ frontend/src/type.d.ts
@@ -0,0 +1,21 @@
+interface IError {
+  code: number;
+  message: string;
+}
+
+interface IToken {
+  token: string
+};
+
+interface cahsOutOrIn {
+  id: number;
+  debitedAccountId: number;
+  creditedAccountId: number;
+  value: number;
+  createdAt: date
+};
+
+interface TransactionSend {
+  creditedAccountId: number;
+  value: number;
+}
