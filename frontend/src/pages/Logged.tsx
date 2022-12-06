94e35c2222054ca201ed19000f2edffc67e6f2c2
DeividBorges93 <deividmborges@gmail.com>
2022-12-06 18:17:18 -0300

    perf(pages): :construction: inicia refatoração da página de dashboard.

--- frontend/src/pages/Logged.tsx
+++ frontend/src/pages/Logged.tsx
@@ -8,40 +8,52 @@ import '../styles/logged.css';
 export default function Logged() {
   const navigate = useNavigate();
 
-  const [balance, setBalance] = useState<number>();
-  const [cashOuts, setCashOut] = useState<cahsOutOrIn[]>([]);
-  const [cashIns, setCashIn] = useState<cahsOutOrIn[]>([]);
-  const [extracts, setExtracts] = useState<cahsOutOrIn[]>([]);
-  const [sortExtracts, setSortExtracts] = useState<cahsOutOrIn[]>([]);
+  const refCreditedId = useRef<HTMLInputElement>(null);
+  const refValueTransfer = useRef<HTMLInputElement>(null);
+
+  const [user, setUser] = useState<UserAllFields>();
+  const [extracts, setExtracts] = useState<Transaction[]>([]);
+
+  const [cashOuts, setCashOut] = useState<Transaction[]>([]);
+  const [cashIns, setCashIn] = useState<Transaction[]>([]);
+  const [sortExtracts, setSortExtracts] = useState<Transaction[]>([]);
 
   // const [formatedDate, setFormatedDate] = useState<string>();
 
-  const getBalanceURL = 'http://localhost:3001/user/balance';
-  const getCashOutURL = 'http://localhost:3001/transactions/debited';
-  const getCashInURL = 'http://localhost:3001/transactions/credited';
+  const getUserInfoURL = 'http://localhost:3001/user/info';
+  const getTransactionsURL = 'http://localhost:3001/transactions/';
+  // const getCashOutURL = 'http://localhost:3001/transactions/debited';
+  // const getCashInURL = 'http://localhost:3001/transactions/credited';
   const sendTransferURL = 'http://localhost:3001/transaction';
   
   const token = localStorage.getItem('token');
   const Authorization = JSON.parse(token || '');
 
   useEffect(() => {
-    axios.get(getBalanceURL, { headers: { Authorization }})
+    axios.get(getUserInfoURL, { headers: { Authorization }})
       .then((response) => {
-        setBalance(response.data);
+        setUser(response.data);
       })
       .catch((err) => err);
-  }, [Authorization]);
+  }, []);
 
-  const username = JSON.parse(localStorage.getItem('username') || '');
+
+  const generateExtract = async () => {
+    if (!user) return;
+
+    const {creditedTransactions, debitedTransactions} = user.account;
+
+    setCashIn(creditedTransactions);
+    setCashOut(debitedTransactions);
+
+    setExtracts([...cashIns, ...cashOuts]);
+  };
 
   const logout = async () => {
     navigate('/user/login');
     localStorage.setItem('token', JSON.stringify(''));
     localStorage.setItem('username', JSON.stringify(''));
   };
-
-  const refCreditedId = useRef<HTMLInputElement>(null);
-  const refValueTransfer = useRef<HTMLInputElement>(null);
 
   const sendTransfer = async (transaction: TransactionSend) => {
     await axios.post(sendTransferURL, transaction, { headers: { Authorization }})
@@ -61,73 +73,13 @@ export default function Logged() {
     }
   };
 
-  const generateExtract = async () => {
-    setCashOut([]);
-    setCashIn([]);
-    setSortExtracts([]);
-
-    const cashOutExtracts = await axios.get(getCashOutURL, { headers: { Authorization }})
-      .then((response) => response.data)
-      .catch((err) => err);
-
-    const cashInExtracts = await axios.get(getCashInURL, { headers: { Authorization }})
-      .then((response) => response.data)
-      .catch((err) => err);
-
-    setExtracts([...cashOutExtracts, ...cashInExtracts])
-  };
-
-  const generateExtractCashIn = async () => {
-    setCashOut([]);
-    setExtracts([]);
-    setSortExtracts([]);
-
-    await axios.get(getCashInURL, { headers: { Authorization }})
-      .then((response) => {
-        setCashIn(response.data);
-      })
-      .catch((err) => err);
-  };
-
-  const generateExtractCashOut = async () => {
-    setCashIn([]);
-    setExtracts([]);
-    setSortExtracts([]);
-
-    await axios.get(getCashOutURL, { headers: { Authorization }})
-      .then((response) => {
-        setCashOut(response.data);
-      })
-      .catch((err) => err);
-  };
-
-  const filterByDateOrder = async () => {
-    if (extracts.length > 0) {
-      const orderned = extracts.reverse();
-      setExtracts([]);
-      setSortExtracts(orderned)
-    };
-
-    if (cashIns.length > 0) {
-      const orderned = cashIns.reverse();
-      setCashIn([]);
-      setSortExtracts(orderned);
-    };
-
-    if (cashOuts.length > 0) {
-      const orderned = cashOuts.reverse();
-      setCashOut([]);
-      setSortExtracts(orderned)
-    };
-
-  };
 
   return (
     <div className="container">
       <div className="container-logged">
         <header className="header-logged">
           <div className="header-left">
-            <span className="actual-balance">{`Saldo atual IT$ ${balance}`}</span>
+            <span className="actual-balance">{`Saldo atual IT$ ${user?.account.balance}`}</span>
           </div>
           <div className="header-center">
             <h1 className="transaction-title">Transferência</h1>
@@ -135,7 +87,7 @@ export default function Logged() {
               <div className="wrap-transaction-input">
                 <input className="input-transaction debited-account-id"
                   type="text"
-                  value={username}
+                  value={user?.username}
                   readOnly
                 />
               </div>
@@ -162,18 +114,20 @@ export default function Logged() {
           </div>
           <div className="header-right">
             <div className="actual-username">
-              <span className="username">{`Logado com ${username}`}</span>
+              <span className="username">{`Logado com ${user?.username}`}</span>
             </div>
-            <button className="logout-btn" onClick={ logout }>Sair</button>
+            <div className="container-logout-btn">
+              <button className="logout-btn" onClick={ logout }>Sair</button>
+            </div>
           </div>
         </header>
         <div className="extracts-container">
           <h1 className="extract-title">Relatório de transações IT Ca$h</h1>
             <div className="extract-btn">
-              <button id="change-order-btn" onClick={filterByDateOrder}>Ordem crescente</button>
+              {/* <button id="change-order-btn" onClick={filterByDateOrder}>Ordem crescente</button> */}
               <button id="general-extract-btn" onClick={generateExtract}>Extrato geral</button>
-              <button id="cashin-extract-btn" onClick={generateExtractCashIn}>Extrato entradas</button>
-              <button id="cashout-extract-btn" onClick={generateExtractCashOut}>Extrato saídas</button>
+              {/* <button id="cashin-extract-btn" onClick={generateExtractCashIn}>Extrato entradas</button>
+              <button id="cashout-extract-btn" onClick={generateExtractCashOut}>Extrato saídas</button> */}
             </div>
           <div className="table-extracts">
             <table id="extract-list">
@@ -189,11 +143,7 @@ export default function Logged() {
               </tr>
             </thead>
             <tbody id="tbody-extract">
-            
-            {extracts.length >= 0 && extracts.map((extract) => RowGenerate(extract))}
-            {cashOuts.length >= 0 && cashOuts.map((cashout) => RowGenerate(cashout))}
-            {cashIns.length >= 0 && cashIns.map((cashin) => RowGenerate(cashin))}
-            {sortExtracts.length >= 0 && sortExtracts.map((sortExtract) => RowGenerate(sortExtract))}
+            {extracts.length >= 0 && extracts.map((extract) => RowGenerate(extract, user?.username))}
             </tbody>
           </table>
           </div>
